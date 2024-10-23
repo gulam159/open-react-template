c0853b73b83ab2d787d85611c281a60979fd71d6
gulam159 <gulammohi159@outlook.com>
2024-10-23 20:00:02 +0530

    feat: add gtm script

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* <!-- Google Tag Manager --> */}
+        <script>
+          {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PVM78DWK');`}
+        </script>
+        {/* <!-- End Google Tag Manager --> */}
         {/* <!-- Google tag (gtag.js) --> */}
         <script
           async
@@ -67,6 +76,15 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${nacelle.variable} bg-gray-950 font-inter text-base text-gray-200 antialiased`}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PVM78DWK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }} // Updated to use JSX style object
+          ></iframe>
+        </noscript>
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
           {children}
